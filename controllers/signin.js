d3e383c21d1f928616e2470f77aba09c27e1cde9
Juan Ovalle <juanovalle9@gmail.com>
2020-04-19 22:38:33 -0400

    Implement input validation

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,12 +1,16 @@
 const handleSignin = (db, bcrypt) => (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json('Incorrect form submission');
+    }
     db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
+        .where('email', '=', email)
         .then(data => {
-            bcrypt.compare(req.body.password, data[0].hash, function(err, isValid) {
+            bcrypt.compare(password, data[0].hash, function(err, isValid) {
                 if (err) throw err;
                 if (isValid) {
                     return db.select('*').from('users')
-                            .where('email', '=', req.body.email)
+                            .where('email', '=', email)
                             .then(user => {
                                 res.json(user[0]);
                             })
