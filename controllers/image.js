41344e0504a7526b0c4d3e7c3f5ec9f7a463ebf5
Juan Ovalle <juanovalle9@gmail.com>
2020-04-19 23:06:10 -0400

    Add then and catch method to handleapicall

--- controllers/image.js
+++ controllers/image.js
@@ -6,7 +6,11 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res) => {
     app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json("Unable to work with API"));
 }
 
 const handleImage = (db) => (req, res) => {
