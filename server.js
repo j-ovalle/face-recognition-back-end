c35821b05e1ba133575725fa01aef2ceb22a018f
Juan Ovalle <juanovalle9@gmail.com>
2020-04-19 23:48:26 -0400

    Fix undefined variable

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send('App working') })
 
 app.post('/signin', signin.handleSignin(db, bcrypt));
 
